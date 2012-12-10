27e8803fee80b3d59a702bc1d0e9af0e8fdd12f8
Jerry Lindahl <jerry@copypasteit.se>
2012-12-10 20:27:19 +0100

    Down works :)

--- Game.hs
+++ Game.hs
@@ -14,14 +14,13 @@ tick state = do
     return $ State.move state 0 1
 
 -- FIXME: this state keeping is messed up
--- FIXME: this if else is not very functional...
 keyPress :: String -> State -> IO State
 keyPress key state | trace ("keyPress (" ++ show key ++ ", ...)") False = undefined
                    | otherwise = do
     case key of
 		"Left"  -> return $ State.setPiece state (rotatePieceACW $ fromJust (State.getPiece state)) (State.getPosition state)
 		"Right" -> return $ State.setPiece state (rotatePieceCW $ fromJust (State.getPiece state)) (State.getPosition state)
-		"Down"  -> undefined
+		"Down"  -> tick state
 		_       -> return state
 
 
